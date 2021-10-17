5932e2b1cd3dbd344c87dd145822c9a96a6f1b05
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2021-10-17 17:39:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-const canvas = require('canvas');
+const Canvas = require('canvas');
+Canvas.registerFont(`${__dirname}/assets/fonts/theboldfont.ttf`, { family: "Bold" });
+Canvas.registerFont(`${__dirname}/assets/fonts/SketchMatch.ttf`, { family: "SketchMatch" });
+Canvas.registerFont(`${__dirname}/assets/fonts/LuckiestGuy-Regular.ttf`, { family: "luckiest guy" });
+Canvas.registerFont(`${__dirname}/assets/fonts/KeepCalm-Medium.ttf`, { family: "KeepCalm" });
